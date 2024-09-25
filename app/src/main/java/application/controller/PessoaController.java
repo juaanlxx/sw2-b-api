a8631d1ec279a9d5816697d7d9d7f5392f60aafd
Juan Pablo <juan.sonsin@etec.sp.gov.br>
2024-09-25 11:18:56 +0000

    save2

--- app/src/main/java/application/controller/PessoaController.java
+++ app/src/main/java/application/controller/PessoaController.java
@@ -1,8 +1,11 @@
 package application.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +27,19 @@ public class PessoaController {
     @PostMapping
     public Pessoa insert(@RequestBody Pessoa novoRegistro){
         return pessoaRepo.save(novoRegistro);
+    }
+
+    @PutMapping("/{id}")
+    public Pessoa update(@PathVariable long id, @RequestBody Pessoa dadosNovos){
+        Pessoa dadosBanco = pessoaRepo.findById(id).get();
+        dadosBanco.setNome(dadosNovos.getNome());
+        dadosBanco.setIdade(dadosNovos.getIdade());
+        
+        return pessoaRepo.save(dadosBanco);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id){
+        pessoaRepo.deleteById(id);
     }
 }
